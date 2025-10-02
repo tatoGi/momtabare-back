6330624b0ec309811617693d720ebf38bf58c972
tatoGI <workspace.tato@gmail.com>
2025-10-02 16:41:42 +0400

    remove cors midllware in api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\CorsMiddleware::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
