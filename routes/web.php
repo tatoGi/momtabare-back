cd67f9f19f51d6c93d765a8452ec2aeccc6d0a28
tatoGI <workspace.tato@gmail.com>
2025-10-02 16:46:26 +0400

    change admin route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\Auth\AdminAuthController;
 
-   Route::get('/', [DashboardController::class, 'index'])
+   Route::get('/admin', [DashboardController::class, 'index'])
         ->name('admin.dashboard')->middleware('auth');
 // Admin authentication routes (only accessible to guests)
 Route::middleware(['guest', 'web'])->prefix('admin')->group(function () {
