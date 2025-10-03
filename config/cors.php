8582489173526ea745d596814c2038e546e1fd75
tatoGI <workspace.tato@gmail.com>
2025-10-04 00:53:49 +0400

    change cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://momtabare-front.vercel.app',
-        'https://admin.momtabare.com'
+        'https://admin.momtabare.com',
+        'https://www.momtabare.com'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
