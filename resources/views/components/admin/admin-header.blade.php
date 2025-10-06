c19e1237a99c684a2da9c269da3c3b612070b3b6
tatoGI <workspace.tato@gmail.com>
2025-10-06 21:52:15 +0400

    add

--- resources/views/components/admin/admin-header.blade.php
+++ resources/views/components/admin/admin-header.blade.php
@@ -1,5 +1,6 @@
 <div class="fixed w-full z-30 flex bg-white dark:bg-[#0F172A] p-2 items-center justify-center h-16 px-10">
-    <div class="logo ml-12 dark:text-white transform ease-in-out duration-500 flex-none h-full flex items-center justify-center">
+    <div
+        class="logo ml-12 dark:text-white transform ease-in-out duration-500 flex-none h-full flex items-center justify-center">
         {{ __('admin.Admin_Panel') }}
     </div>
     <!-- SPACER -->
@@ -7,23 +8,27 @@
     <div class="flex-none h-full text-center flex items-center justify-center">
         <div class="flex space-x-3 items-center px-3">
             <div class="flags flex">
-                
-                @foreach(locales() as $locale => $data)
-                    @if($locale === 'en')
+
+                @foreach (locales() as $locale => $data)
+                    @if ($locale === 'en')
                         <!-- English Flag Icon -->
-                        <a href="{{ $data }}" class="block h-4 mt-4 px-2 text-[#7f8890] hover:text-black transition-colors duration-300" title="{{ __('English') }}">
-                           {{__('admin.english')}}
+                        <a href="{{ $data }}"
+                            class="block h-4 mt-4 px-2 text-[#7f8890] hover:text-black transition-colors duration-300"
+                            title="{{ __('English') }}">
+                            {{ __('admin.english') }}
                         </a>
                     @elseif($locale === 'ka')
                         <!-- Georgian Flag Icon -->
-                        <a href="{{ $data }}" class="block h-4 mt-4 px-2 text-[#7f8890] hover:text-black transition-colors duration-300" title="{{ __('Georgian') }}">
-                          {{__('admin.georgian')}}
+                        <a href="{{ $data }}"
+                            class="block h-4 mt-4 px-2 text-[#7f8890] hover:text-black transition-colors duration-300"
+                            title="{{ __('Georgian') }}">
+                            {{ __('admin.georgian') }}
                         </a>
                     @endif
                 @endforeach
             </div>
             <ul class="flex list-outside hover:list-inside">
-                
+
                 {{-- <li class="mr-12 inline-block">
                     <a href="#" class="block h-8 mt-4 px-15 text-[#7f8890] hover:text-black  transition-colors duration-300" title="{{ __('Blog Comments') }}" data-original-title="{{ __('Blog Comments') }}">
                         <i class="ico fas fa-comment-alt text-xl"></i>
@@ -37,41 +42,46 @@
                     {{ $contactMessageCount }}
                 </div>
                 <li class="w-8 mr-12 inline-block">
-                    <a href="/{{ app()->getlocale() }}/admin/contact" class="block h-8 mt-4 px-15 text-[#7f8890] hover:text-black  transition-colors duration-300" title="{{ __('Contacts') }}">
+                    <a href="/{{ app()->getlocale() }}/admin/contact"
+                        class="block h-8 mt-4 px-15 text-[#7f8890] hover:text-black  transition-colors duration-300"
+                        title="{{ __('Contacts') }}">
                         <i class="ico fas fa-envelope text-xl"></i>
                     </a>
                 </li>
             </ul>
-            
+
             <div class="w-8 flex-none flex justify-center relative">
                 <div class="w-8 h-8 flex">
-                    
+
                 </div>
-              <!-- Dropdown menu -->
-                <div id="mobileDropdown" class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10 hidden cursor-pointer">
+                <!-- Dropdown menu -->
+                <div id="mobileDropdown"
+                    class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10 hidden cursor-pointer">
                     <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                         {{ __('admin.Profile') }}
                     </a>
                     <form method="POST" action="{{ route('admin.logout', app()->getLocale()) }}" class="w-full">
                         @csrf
-                        <button type="submit" class="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        <button type="submit"
+                            class="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                             {{ __('admin.Logout') }}
                         </button>
                     </form>
                 </div>
             </div>
-            
-            <div class="md:block text-sm md:text-md text-black dark:text-white float-right cursor-pointer relative" onclick="toggleDropdown()">
-                {{ Auth::user()->name }}
+
+            <div class="md:block text-sm md:text-md text-black dark:text-white float-right cursor-pointer relative"
+                onclick="toggleDropdown()">
+                {{ Auth::user()->name ?? '' }}
             </div>
-            
+
             <script>
                 function toggleDropdown() {
                     var dropdown = document.getElementById('mobileDropdown');
                     dropdown.classList.toggle('hidden');
                 }
             </script>
-            
+
         </div>
     </div>
 </div>
