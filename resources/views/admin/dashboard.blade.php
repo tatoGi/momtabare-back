35bbe28369c54c74af7d03a84cac55d703543599
tatoGI <workspace.tato@gmail.com>
2025-10-10 03:14:03 +0400

    admin leyout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-admin.Admin-layout>
+<x-admin.admin-layout>
     <x-slot name="title">Dashboard</x-slot>
     <!-- Dashboard content goes here -->
-</x-admin.Admin-layout> 
+</x-admin.admin-layout> 
