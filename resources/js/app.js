8e48b4cce5349253d455cda00b824effd4d3e0e6
tatoGI <workspace.tato@gmail.com>
2025-10-09 22:56:55 +0400

    changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
-
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+window.ClassicEditor = ClassicEditor;
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
