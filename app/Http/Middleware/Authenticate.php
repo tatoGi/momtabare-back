55640fe9238b1b3c3e5dac781a22e21d18174a1a
tatoGI <workspace.tato@gmail.com>
2025-10-09 14:19:06 +0400

    changes

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -19,29 +19,10 @@ class Authenticate extends Middleware
      *
      * @throws \Illuminate\Auth\AuthenticationException
      */
-    public function handle($request, Closure $next, ...$guards)
+    protected function redirectTo($request)
     {
-      
-    
-        if ($request->routeIs('admin.login') || $request->routeIs('admin.login.submit')) {
-            return $next($request);
-        }
-    
-        try {
-            dd($request, $next, $guards);
-            return parent::handle($request, $next, ...$guards);
-        } catch (\Exception $e) {
-          
-            
-            Auth::guard('web')->logout();
-            $request->session()->invalidate();
-            $request->session()->regenerateToken();
-            
-            if ($request->expectsJson()) {
-                return response()->json(['message' => 'Unauthenticated.'], 401);
-            }
-    
-            return redirect(route('admin.login', ['locale' => app()->getLocale()]));
+        if (! $request->expectsJson()) {
+            return route('admin.login', ['locale' => app()->getLocale()]);
         }
     }
 }
--- config/session.php
+++ config/session.php
@@ -154,7 +154,7 @@ return [
 
     'path' => '/',
     'domain' => env('SESSION_DOMAIN', null),
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
     'http_only' => true,
     'same_site' => 'lax',
 
