0b0ba37a5087ff4c88e9479d5caa7c9144355061
tatoGI <workspace.tato@gmail.com>
2025-10-10 02:51:01 +0400

    include app.js

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/admin-login.css') }}">
-
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Scripts -->
 </head>
 
