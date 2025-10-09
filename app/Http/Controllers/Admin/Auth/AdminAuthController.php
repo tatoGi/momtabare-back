766b31c3f15137811ada862620deb3663e50ecb5
tatoGI <workspace.tato@gmail.com>
2025-10-09 19:37:30 +0400

    dd

--- app/Http/Controllers/Admin/Auth/AdminAuthController.php
+++ app/Http/Controllers/Admin/Auth/AdminAuthController.php
@@ -52,7 +52,7 @@ class AdminAuthController extends Controller
             Auth::login($user, $request->filled('remember'));
            
             // ✅ Redirect to admin dashboard
-            return redirect(url('/' . app()->getLocale() . '/admin'));
+            return view('admin.dashboard');
         }
     
         // ❌ If authentication fails
