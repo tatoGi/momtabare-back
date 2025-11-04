2d2644d408d4a4a67177f5027ab0042be46ef428
tatoGI <workspace.tato@gmail.com>
2025-11-04 20:31:13 +0400

    fix: Fix case-sensitive namespace for FrontendController

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\RateProductController;
 use App\Http\Controllers\Api\NotificationController;
 use App\Http\Controllers\Api\PromoCodeController;
-use App\Http\Controllers\website\FrontendController;
+use App\Http\Controllers\Website\FrontendController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
